e9a824422cfa645ee8e838512ceeccc1dd727881
Fedor Levin <levinfm6@gmail.com>
2021-03-23 18:08:53 -0400

    new macro fun

--- macro_indicators_toolkit.py
+++ macro_indicators_toolkit.py
@@ -1,23 +1,23 @@
 from fredapi import Fred
-fred = Fred(api_key='YOUR API')
+fred = Fred(api_key='9bb5bcafdc457fccd015003c27c46e93')
 
 def treasury_ten_year():
     """
-    Returns: 10-Year Treasury Constant Maturity Rate
+    Returns: 10-Year Treasury Constant Maturity Rate; Unit: percent per annum
     """
     treasury_lt = fred.get_series('DGS10')
     return treasury_lt
 
 def treasury_one_year():
     """
-    Returns: 1-Year Treasury Constant Maturity Rate
+    Returns: 1-Year Treasury Constant Maturity Rate; Unit: percent per annum
     """
     treasury_st = fred.get_series('DGS1')
     return treasury_st
 
 def treasury_three_month():
     """
-    Returns: 3-Month Treasury Constant Maturity Rate
+    Returns: 3-Month Treasury Constant Maturity Rate; Unit: percent per annum
     """
     treasury_stm = fred.get_series('DGS3MO')
     return treasury_stm
@@ -111,7 +111,125 @@ def plot_spread_recession():
     ax = spr.plot(figsize=(12,6))
     (rec['1981':]*5).plot(ax=ax, style="--", linewidth=3)
 
+def tenyear_cm_minus_fedfunds():
+    """
+    10-Year Treasury Constant Maturity Minus Federal Funds Rate, Monthly
+    """
+    ttenyffm = fred.get_series('T10YFFM')
+    ttenyffm.dropna(inplace=True)
+    return ttenyffm
 
+def three_month_tbill_minus_fedfunds():
+    """
+    3-Month Treasury Bill Minus Federal Funds Rate, Monthly
+    """
+    tbthreemffm = fred.get_series('TB3SMFFM')
+    tbthreemffm.dropna(inplace=True)
+    return tbthreemffm
+
+def all_employee():
+    """
+    All Employees, Total Nonfarm
+    """
+    nonfpayems = fred.get_series('PAYEMS')
+    nonfpayems.dropna(inplace=True)
+    return nonfpayems
+
+def job_open():
+    """
+    Job Openings: Total Nonfarm, Monthly
+    """
+    jobopen = fred.get_series('JTSJOL')
+    jobopen.dropna(inplace=True)
+    return jobopen
+
+def five_year_cm_minus_fedfund():
+    """
+    5-Year Treasury Constant Maturity Minus Federal Funds Rate, Monthly
+    """
+    fiveyffm = fred.get_series('T5YFFM')
+    fiveyffm.dropna(inplace=True)
+    return fiveyffm
+
+def one_year_cm_minus_fedfund():
+    """
+    1-Year Treasury Constant Maturity Minus Federal Funds Rate, Monthly
+    """
+    oneyffm = fred.get_series('T1YFFM')
+    oneyffm.dropna(inplace=True)
+    return oneyffm
+
+def us_uk_fx():
+    """
+    U.S. / U.K. Foreign Exchange Rate, U.S. Dollars to One British Pound, Monthly
+    """
+    usukexch = fred.get_series('EXUSUK')
+    usukexch.dropna(inplace=True)
+    return usukexch
+
+def housing_stats_west_census():
+    """
+    Housing Starts in West Census Region, Thousands of Units, Monthly
+    """
+    house = fred.get_series('HOUSTW')
+    house.dropna(inplace=True)
+    return house
+
+def three_month_trs_secondary():
+    """
+    3-Month Treasury Bill: Secondary Market Rate, Monthly
+    """
+    threems = fred.get_series('TB3MS')
+    threems.dropna(inplace=True)
+    return threems
+
+def employee_constr():
+    """
+    All Employees, Construction, Monthly
+    """
+    usconstr = fred.get_series('USCONS')
+    usconstr.dropna(inplace=True)
+    return usconstr
+
+def overtime_manuf():
+    """
+    Average Weekly Overtime Hours of Production and Nonsupervisory Employees, Manufacturing, Monthly
+    """
+    overtime = fred.get_series('AWOTMAN')
+    overtime.dropna(inplace=True)
+    return overtime
+
+def housing_owened():
+    """
+    Housing Starts: Total: New Privately Owned Housing Units Started, Monthly
+    """
+    housest = fred.get_series('HOUST')
+    housest.dropna(inplace=True)
+    return housest
+
+def employee_durable():
+    """
+    All Employees, Durable Goods, Monthly
+    """
+    durgood = fred.get_series('DMANEMP')
+    durgood.dropna(inplace=True)
+    return durgood
+
+def employee_service():
+    """
+    All Employees, Service-Providing, Monthly
+    """
+    survp = fred.get_series('SRVPRD')
+    survp.dropna(inplace=True)
+    return survp
+
+def initial_claims():
+    """
+    Initial Claims, Monthly
+    """
+    claim = fred.get_series('ICSA')
+    claim.dropna(inplace=True)
+    return claim
 
 
 
